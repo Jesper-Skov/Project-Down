60a358307478b9c3021b3b87687bee77de9b4de7
Jesper-Skov <31208883+Jesper-Skov@users.noreply.github.com>
2017-08-21 14:00:21 +0200

    Upload af filer

---
+++ Webside/javascript/down.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+/*Har fundet denne kode fra w3school for at lave side menuen til smartphones*/
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
